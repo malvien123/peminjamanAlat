e78bff1e1d0c091c9f73ffde93ce383abd7571df
malvien123 <muhamadmalvien@gmail.com>
2026-04-24 18:47:37 +0700

    update

--- view/v_peminjaman_petugas.php
+++ view/v_peminjaman_petugas.php
@@ -31,7 +31,7 @@ include '../controller/c_peminjaman.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         @media print { 
-            .no-print { display: none !important; } 
+           .btn , .btn ,  .no-print {  display: none !important; } 
             .badge { color: black !important; border: 1px solid #ccc; }
         }
     </style>
@@ -40,7 +40,7 @@ include '../controller/c_peminjaman.php';
     <div class="container bg-white p-4 shadow rounded">
         <div class="d-flex justify-content-between mb-4 align-items-center">
             <h2 class="m-0"><center>Laporan  Peminjaman Alat</h2>
-            <a href="../controller/c_login.php?aksi=logout" onclick="return confirm('Apakah Anda yakin ingin keluar dari sesi?');" class="btn btn-danger">logout</a>
+            <a href="../controller/c_login.php?aksi=logout" onclick="return confirm('Apakah Anda yakin ingin keluar dari sesi?');" class="btn btn-danger" >logout</a>
             <button onclick="window.print()" class="btn btn-secondary no-print" href="v_cetak_laporan.php">Cetak Laporan</button>
         </div>
 
